d8df352ad66b91f45d07882e49ed73b7dc74446a
Ziga Paradzik <nubis@Zigas-MacBook-Pro.local>
2018-11-22 16:30:55 +0700

    CSS Cards and interface basics done

--- game.js
+++ game.js
@@ -79,7 +79,6 @@ app.table = {
 	cardsLeft: []
 }
 
-
 app.deck =  {
 	cards: [
 		guard = {
@@ -421,8 +420,37 @@ app.init = function() {
 // RUN AFTER DOM LOADS
 $(function() {
     app.init();
+    autoSizeText();
 });
 
 
+// RESIZE FUNCTION -- OFF THE NET -- ADD .RESIZE CLASS TO SPANS WITH LONG NAME CARDS
 
+var autoSizeText;
+autoSizeText = function() {
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
 
